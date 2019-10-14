2533d5921692a3eb46f88bdad12d7ef4d1d9fc80
AxelAlavedra <axel.alavedra@gmail.com>
2019-10-14 17:09:42 +0200

    Debug face normals

--- SparkEngine/Globals.h
+++ SparkEngine/Globals.h
@@ -20,6 +20,7 @@ void log(const char file[], int line, const char* format, ...);
 #define DEGTORAD 0.0174532925199432957f
 #define RADTODEG 57.295779513082320876f
 #define HAVE_M_PI
+#define DEBUG_NORMAL_LENGTH 0.2f
 
 typedef unsigned int uint;
 
