6555b07324db57b4a8be21a355f5f05bdbe4c4b5
Axel Alavedra Cabello <axel.alavedra@gmail.com>
2019-10-25 10:25:31 +0200

    Vsync swap working ?

--- SparkEngine/ModuleRenderer3D.cpp
+++ SparkEngine/ModuleRenderer3D.cpp
@@ -357,16 +357,21 @@ void ModuleRenderer3D::CreateSceneBuffer()
 
 	glGenTextures(1, &scene_texture_id);
 	glBindTexture(GL_TEXTURE_2D, scene_texture_id);
+
 	float2 size = float2(App->window->GetWindowWidth(), App->window->GetWindowHeight());
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, size.x, size.y, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, scene_texture_id, 0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, scene_texture_id, 0);
 
 	glGenRenderbuffers(1, &scene_depth_id);
 	glBindRenderbuffer(GL_RENDERBUFFER, scene_depth_id);
 	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, size.x, size.y);
 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, scene_depth_id);
+	glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
