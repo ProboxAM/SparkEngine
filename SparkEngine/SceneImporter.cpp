b4b97ea84d92ac9a9263f6253306f3c78d16e329
AxelAlavedra <axel.alavedra@gmail.com>
2019-11-06 18:37:05 +0100

    Update SceneImporter.cpp

--- SparkEngine/SceneImporter.cpp
+++ SparkEngine/SceneImporter.cpp
@@ -113,7 +113,7 @@ void SceneImporter::LoadNode(const aiNode* node, const aiScene* scene, GameObjec
 		Texture* new_texture;
 		aiMesh* current_mesh = scene->mMeshes[node->mMeshes[0]];
 
-		new_mesh = App->importer->mesh->Load("mesh.spk");
+		new_mesh = App->importer->mesh->Import(scene, current_mesh);
 		new_object->bounding_box.SetNegativeInfinity();
 		new_object->bounding_box.Enclose(new_mesh->vertices, new_mesh->total_vertices);
 
